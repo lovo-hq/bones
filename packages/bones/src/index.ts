97bed0413ee85c1df91ff8f460e6168ca3b6a1b1
Hunter Garrett <hunter@hunterbecton.com>
2026-04-21 20:11:28 -0400

    refactor: remove useBone, useBones, Bones, Streamable, and context

--- packages/bones/src/index.ts
+++ packages/bones/src/index.ts
@@ -1,6 +1,2 @@
 export { createBones, readPromise } from "./create-bones.ts";
-export type {
-  BoneType,
-  BoneOptions,
-  CreateBonesReturn,
-} from "./create-bones.ts";
+export type { BoneType, BoneOptions, CreateBonesReturn } from "./create-bones.ts";
--- packages/bones/src/use-bones.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { BonesContext } from "./context.ts";
-
-export function useBones() {
-  const ctx = useContext(BonesContext);
-  return { isLoading: ctx.forced };
-}
